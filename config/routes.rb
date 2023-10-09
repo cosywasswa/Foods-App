d856a737056d9b977bde5b17b74e42e2e39caa27
Cosmas Wasswa <cosywas@gmail.com>
2023-10-09 11:49:04 +0300

    Food-App setup

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :recipes, only: [:index, :show, :new, :create, :destroy]
+    resources :foods, only: [:index, :show, :new, :create, :destroy]
+    resources :recipe_foods, except: %i[edit update]
+    resources :public_recipes, only: [:index]
+    resources :shopping_list, only: [:index, :show]
+  end
+end
