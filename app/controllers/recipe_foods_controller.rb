5cea8e996a5bc26dd443f0ff8cdcb31f3274ff6c
AJ193 <119049613+AJ193@users.noreply.github.com>
2023-10-09 14:19:00 +0500

    Add files

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,19 @@
+class RecipeFoodsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def modify
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+  get 'shopping_list/index'
+  get 'shopping_list/show'
+  get 'foods/index'
+  get 'foods/show'
+  get 'foods/new'
+  get 'foods/create'
+  get 'foods/destroy'
+  get 'public_recipes/index'
+  get 'public_recipes/show'
+  get 'recipe_foods/index'
+  get 'recipe_foods/show'
+  get 'recipe_foods/new'
+  get 'recipe_foods/create'
+  get 'recipe_foods/modify'
+  get 'recipe_foods/destroy'
+  get 'recipe/index'
+  get 'recipe/show'
+  get 'recipe/new'
+  get 'recipe/create'
+  get 'recipe/destroy'
+  get 'users/index'
+  get 'users/show'
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :recipes, only: [:index, :show, :new, :create, :destroy]
