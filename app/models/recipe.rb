78951e3f983e125b18f265e3afacf97293aabdcc
Cosmas Wasswa <cosywas@gmail.com>
2023-10-10 21:26:58 +0300

    Add thr total_price

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
 
   validates :name, :preparation_time, :cooking_time, :description, presence: true
+  def total_price
+    recipe_foods.sum { |fd| fd.quantity * fd.food.price }
+  end
 end
