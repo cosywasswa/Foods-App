026877aa75c81c64969bacebc127e8d11b6af15b
Cosmas Wasswa <cosywas@gmail.com>
2023-10-12 21:01:04 +0300

    Updated public recipe to fetch only public recipes

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -3,7 +3,6 @@ class PublicRecipesController < ApplicationController
     @user = current_user
     @public_recipes = Recipe.includes(:user)
       .where(public: true)
-      .or(Recipe.where(user_id: current_user))
       .order(created_at: :desc)
   end
 
